a912460a2ad4275c77846350b83292c772004cc9
selimjabbar <selim@jabbar.info>
2025-03-26 04:27:32 +0300

    Update api.js

--- src/util/api.js
+++ src/util/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const apiUrl = "https://154.205.134.245/api"
 //const apiUrl="http://127.0.0.1:8000"
-const apiUrl="https://apple--icloud.com"
+const apiUrl="https://jadarat.org"
 
 const apiPostHandle = async (url, data) => {
     return await axios.post(apiUrl + url, data, {
